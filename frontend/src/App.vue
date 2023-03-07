74fb80a6ec5b2a4d9a168a25980edd29caffaead
hrtpng <irynastrybailo@gmail.com>
2023-03-07 10:36:12 +0100

    added styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,30 @@
     <h2>KATALOG ODCZYNNIKÓW</h2>
     <div class="buttons">
       <button>ZALOGUJ SIĘ</button>
-      <button>KONTAKT</button>
+      <button @click="showModal=true">KONTAKT</button>
     </div>
+  </div>
+  <div v-if="showModal">
+    <div class="modal-mask">
+          <div class="modal-wrapper">
+            <div class="modal-container">
+
+              <div class="modal-header">
+                <slot name="header">KONTAKT</slot>
+              </div>
+
+              <div class="modal-body">
+                <slot name="body">Dla zgłoszenia problemu na stronie proszę pisać na istr@umk.pl</slot>
+              </div>
+
+              <div class="modal-footer">
+                <slot name="footer">
+                  <button class="modal-default-button" @click="showModal=false">OK</button>
+                </slot>
+              </div>
+            </div>
+          </div>
+        </div>
   </div>
   <div class="title">KChMAiK UMK.Pracownia 94-95
     <button class="btn-another-storage">Zmień</button>
@@ -37,7 +59,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="compound in storage" :key="compound.title">
+        <tr v-for="(compound, key) in storage" :key="key">
           <td>{{ compound.formula }}</td>
           <td>{{ compound.title || textIfEmpty }}</td>
           <td class="centered-obj">{{ compound.cas || textIfEmpty }}</td>
@@ -57,7 +79,8 @@ export default {
     return {
       storage: [],
       searchText: '',
-      textIfEmpty: '-'
+      textIfEmpty: '-',
+      showModal: false,
     }
   },
   watch: {
@@ -171,7 +194,7 @@ button:hover {
   padding: 10px 5px;
   margin: 5px;
   width: 98%;
-  
+  font-size: 1.1em;
 }
 
 table {
@@ -194,7 +217,6 @@ table {
 .results td {
   margin: 0;
   border-bottom: 1px solid #004492;
-  /* border-left: 1px solid #004492; */
   padding: 15px;
 }
 
@@ -209,5 +231,55 @@ span {
 h1 {
   color: #004492;
 }
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 500px;
+  height: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #DCE6Fa;
+  border-radius: 4px;
+  font-family: Lato-Regular, Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.modal-header {
+  text-align: center;
+  font-size: 40px;
+  color: #004492;
+  margin-top: 20px;
+}
+
+.modal-body {
+  margin: 20px 0;
+  font-size: 25px;
+  color: #004492;
+
+}
+
+.modal-default-button {
+  float: right;
+  width: 5cm;
+  background-color: #aad23f;
+}
+
 
 </style>
